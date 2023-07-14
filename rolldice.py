ecd27bc1c2371ed2ed30a06180a25a9eff374338
LalitPathak27 <104768342+LalitPathak27@users.noreply.github.com>
2023-07-14 20:57:22 +0530

    Add files via upload

---
+++ rolldice.py
@@ -0,0 +1,30 @@
+import random
+
+response = "y"
+while response == "y":
+    no = random.randint(1,6)
+
+    if no == 1:
+        print("0")
+
+    if no == 2:
+        print("0 0")
+
+    if no == 3:
+        print("0 ") 
+        print("  0 ") 
+        print("    0 ")        
+    if no == 4:
+        print("0 0") 
+        print("0 0") 
+    if no == 5:
+        print("0 0") 
+        print(" 0 ") 
+        print("0 0") 
+    if no == 6:
+        print("0 0 0 ")     
+        print("0 0 0")
+
+    response = input("press y to roll a dice")      
+
+    print("\n")  
